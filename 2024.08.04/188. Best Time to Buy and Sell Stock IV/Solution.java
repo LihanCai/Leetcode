b86e4299432d7dbffd316cc27201a95a99fd8bb3
mwszc <cai.lih@northeastern.edu>
2024-08-04 16:56:48 +0200

    188

---
+++ 2024.08.04/188. Best Time to Buy and Sell Stock IV/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int[] buy = new int[k + 1];
+        int[] sell = new int[k + 1];
+        Arrays.fill(buy, -1000);
+        Arrays.fill(sell, 0);
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = 1; j <= k; j++) {
+                buy[j] = Math.max(buy[j], sell[j - 1] - prices[i]);
+                sell[j] = Math.max(sell[j], buy[j] + prices[i]);
+            }
+        }
+        return sell[k];
+    }
+}
